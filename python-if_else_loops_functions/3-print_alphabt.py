2cf6fb72189c8bfcad4575a9e5851b554e205d21
jocalao001 <joca-001@hotmail.com>
2023-06-01 16:25:45 -0700

    taskthree

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    if chr(i) is not 'q' and chr(i) is not 'e':
-        print("{}".format(chr(i)), end="")
+for alphabet in range(97, 123):
+    if alphabet == 101 or alphabet == 113:
+        continue
+    print("{}".format(chr(alphabet)), end='')
