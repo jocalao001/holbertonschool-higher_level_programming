bf69760b15ab879bdcebfc7c853e4643663eb688
jocalao001 <113933963+jocalao001@users.noreply.github.com>
2023-06-18 21:50:04 -0500

    Update 2-rectangle.py

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -1,20 +1,29 @@
 #!/usr/bin/python3
-""" Class, defines a rectangle"""
+# 2-rectangle.py
+# Brennan D Baraban <375@holbertonschool.com>
+"""Defines a Rectangle class."""
+
+
 class Rectangle:
-    """ Empty class  """
+    """Represent a rectangle."""
 
     def __init__(self, width=0, height=0):
+        """Initialize a new Rectangle.
+
+        Args:
+            width (int): The width of the new rectangle.
+            height (int): The height of the new rectangle.
+        """
         self.width = width
         self.height = height
 
     @property
     def width(self):
-        """ retrieving """
+        """Get/set the width of the Rectangle."""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """Sets the width of the rectangle instance"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -23,7 +32,7 @@ class Rectangle:
 
     @property
     def height(self):
-        """ retrieving """
+        """Get/set the height of the Rectangle."""
         return self.__height
 
     @height.setter
@@ -34,14 +43,12 @@ class Rectangle:
             raise ValueError("height must be >= 0")
         self.__height = value
 
-    """task 2 """
-
     def area(self):
-        """ Calculates the área of a rectangle and returns it """
-        return self.__width * self.__height
+        """Return the area of the Rectangle."""
+        return (self.__width * self.__height)
 
     def perimeter(self):
-        """ Calculates the perimeter of a rectangle """
+        """Return the perimeter of the Rectangle."""
         if self.__width == 0 or self.__height == 0:
-            return 0
-        return 2*(self.__width + self.height)
+            return (0)
+        return ((self.__width * 2) + (self.__height * 2))
